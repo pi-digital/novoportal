75a5f991bbf061a3987c29dfa0ddc34af1648b3c
pi-digital <54288024+pi-digital@users.noreply.github.com>
2020-03-22 09:01:45 -0300

    Update script.js

--- pesquisa/script.js
+++ pesquisa/script.js
@@ -1,13 +1,12 @@
-var data = [
-      {id:1,text:'Apple',img:'http://placehold.it/250x250',link:'http://www.inpi.gov.br/'},
-      {id:2,text:'Microsoft',img:'http://placehold.it/250x250',link:'http://placehold.it/250x250'},
-      {id:3,text:'Google',img:'http://placehold.it/250x250',link:'http://placehold.it/250x250'}
-           ];
 $(document).ready(function() {
   $('input.autocomplete').autocomplete2({
-        data: data,
-        select: function(event, ui) { 
-            window.location.href = ui.item.link;
-                                    }
-                                       });
+        data: [
+      {id:1,text:'Apple',img:'http://placehold.it/250x250',url:'http://www.inpi.gov.br/'},
+      {id:2,text:'Microsoft',img:'http://placehold.it/250x250',url:'http://placehold.it/250x250'},
+      {id:3,text:'Google',img:'http://placehold.it/250x250',url:'http://placehold.it/250x250'}
+    ],
+        select: function( event, ui ) { 
+            window.location.href = ui.item.url;
+        }
+    });
 });
