24b17ecb0af49919e29a594e876037fa889bb77e
pi-digital <54288024+pi-digital@users.noreply.github.com>
2020-06-05 20:30:14 -0300

    Update script.js

--- enderecos/script.js
+++ enderecos/script.js
@@ -91,67 +91,80 @@ imageSeries.data = [ {
   "title": "Sede, INPI (Rio de Janeiro, RJ)",
   "latitude": -22.9110137,
   "longitude": -43.2093727,
-  "color":"#fff"
+  "value": 200,
+  "color": "#000"
 }, {
   "title": "Regional, INPI (São Paulo, SP)",
   "latitude": -23.5506507,
   "longitude": -46.6333824,
-  "color":"#fff"
+  "value": 100,
+  "color": "#808080"
 }, {
   "title": "Regional, INPI (Brasília, DF)",
   "latitude": -15.7934036,
   "longitude": -47.8823172,
-  "color":"#fff"
+  "value": 100,
+  "color": "#808080"
 }, {
   "title": "Regional, INPI (Goiânia, GO)",
   "latitude": -16.680882,
   "longitude": -49.2532691,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional - Recife, PE",
   "latitude": -8.04255435,
   "longitude": -34.92770629,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional - Fortaleza, CE",
   "latitude": -3.7304512,
   "longitude": -38.5217989,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional - Campina Grande, PB",
   "latitude": -7.2233728,
   "longitude": -35.88797621,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional, INPI - Aracaju, SE",
   "latitude": -11.01868955,
   "longitude": -37.0944378,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional, INPI - Belo Horizonte, MG",
   "latitude": -19.9227318,
   "longitude": -43.9450948,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Escritório Regional, INPI - Vitória, ES",
   "latitude": -20.2872412,
   "longitude": -40.32680602,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Regional, INPI - Porto Alegre, RS",
   "latitude": -30.10091665,
   "longitude": -51.18878819,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Regional, INPI - Curitiba, PR",
   "latitude": -25.4295963,
   "longitude": -49.2712724,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 }, {
   "title": "Regional, INPI - Florianópolis, SC",
   "latitude": -27.5973002,
   "longitude": -48.5496098,
-  "color":"#fff"
+  "value": 50,
+  "color": "#fff"
 } ];
 
 // Configure series
