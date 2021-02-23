1fff797c5caf73c62c069fa1b70a163bba8662c6
pi-digital <54288024+pi-digital@users.noreply.github.com>
2021-02-23 18:19:15 -0300

    Create script.js

---
+++ carrossel/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.banner').unslider({
+      //  Enable keyboard arrows
+      keys: true,               
+			// Enable dot nav
+      dots: true,
+      delay: 7000
+    });
+});
